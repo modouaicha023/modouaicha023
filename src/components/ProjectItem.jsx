e7e878e4429779feb3baeb97f5348de799497f3b
Modou Aicha Diop <modouaichadiop@gmail.com>
2024-01-03 13:52:24 +0000

    Fix positioning of Github link

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -28,7 +28,7 @@ export default  function ProjectItem({project}) {
               </span>
             ))}
           </div>
-          <div className="self-end flex gap-1 w-fit absolute right-0 top-6 opacity-80">
+          <div className="self-end flex gap-1 w-fit absolute right-0 -top-6 opacity-80">
             <Link href={project?.githubUrl || "#"} target="_blank">
               <Github size={16} />
             </Link>
