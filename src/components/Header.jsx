f790ed1801e1c1c1039fcca53c5e0e03ce941e34
modouaicha023 <modouaichadiop@gmail.com>
2024-06-15 17:26:56 +0000

    chore: Optimize SEO metadata and add analytics settings

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,13 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import logo from "../../public/images/Logo.png";
-import { MoreHorizontal, Linkedin, Github, Twitter,FileText  } from "lucide-react";
+import {
+  MoreHorizontal,
+  Linkedin,
+  Github,
+  Twitter,
+  FileText,
+} from "lucide-react";
 // import dynamic from "next/dynamic";
 import basketball from "../../public/images/ballBAsket.gif";
 import ThemeToggle from "./ui/utils/ThemeToggle";
@@ -21,18 +27,34 @@ export default function Header() {
       <div className="flex gap-2 justify-center items-center">
         <div className="font-extrabold text-center hidden sm:flex items-center justify-center min-w-[250px] text-3xl">
           <span>modouaicha</span>
-          <Image src={basketball} width={48} height={48} alt="Ball" />
+          <div className="relative w-12 h-12">
+            <Image
+              src={basketball}
+              layout="fill"
+              objectFit="contain"
+              objectPosition="center"
+              alt="Ball"
+            />
+          </div>
           <span>23</span>
         </div>
         <div className="flex gap-1">
-          <Link href="https://github.com/modouaicha023"><Github size={16}/></Link>
-          <Link href="https://www.linkedin.com/in/modouaicha023"><Linkedin size={16}/></Link>
-          <Link href="https://twitter.com/modouaicha023"><Twitter size={16}/></Link>
-          <Link href="https://modouaicha023-cv.vercel.app/"><FileText  size={16}/></Link>
+          <Link href="https://github.com/modouaicha023">
+            <Github size={16} />
+          </Link>
+          <Link href="https://www.linkedin.com/in/modouaicha023">
+            <Linkedin size={16} />
+          </Link>
+          <Link href="https://twitter.com/modouaicha023">
+            <Twitter size={16} />
+          </Link>
+          <Link href="https://modouaicha023-cv.vercel.app/">
+            <FileText size={16} />
+          </Link>
         </div>
       </div>
       <div className="flex items-center justify-center">
-        <LangSwitcher/>
+        <LangSwitcher />
         <ThemeToggle />
         <MoreHorizontal size={48} className="cursor-not-allowed" />
       </div>
--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -3,25 +3,25 @@ import React from "react";
 import Image from "next/image";
 import { Github, ExternalLink } from "lucide-react";
 
-export default  function ProjectItem({project}) {
-    
+export default function ProjectItem({ project }) {
   return (
-    <div className=" min-h-fit  max-w-[450px] bkpgrid:row-start-2 bkpgrid:row-end-6 bkpgrid:col-start-6 bkpgrid:col-end-10 border border-white border-opacity-10 rounded min-w-[250px] flex flex-col gap-4 p-2 shadow-md">
+    <div className="min-h-fit max-w-[450px] bkpgrid:row-start-2 bkpgrid:row-end-6 bkpgrid:col-start-6 bkpgrid:col-end-10 border border-white border-opacity-10 rounded min-w-[250px] flex flex-col gap-4 p-2 shadow-md">
       <div className="min-w-[250px] h-full flex flex-col gap-6 p-2">
         <h2 className="text-xl text-center">{project?.name}</h2>
         <div className="w-full h-full min-h-[250px] p-4 relative">
           <Image
             src={project?.image}
-            className=" w-10/12 absolute border border-white border-opacity-10 object-cover shadow-lg max-h-max"
+            className="w-10/12 absolute border border-white border-opacity-10 object-cover shadow-lg max-h-max"
             fill
+            sizes="(max-width: 450px) 100vw, 450px"
             alt="Project image"
           />
         </div>
       </div>
 
-      <div className="w-full  p-4 flex flex-col gap-1">
-        <div className="flex  gap-x-2 relative">
-          <div className="text-xl flex gap-3 truncate line-clamp-1  ">
+      <div className="w-full p-4 flex flex-col gap-1">
+        <div className="flex gap-x-2 relative">
+          <div className="text-xl flex gap-3 truncate line-clamp-1">
             {project?.stack?.map((tech, index) => (
               <span key={index} className="opacity-40">
                 {tech}
