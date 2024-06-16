08a5335d13476eeeab14294e96731ae3a82e526a
modouaicha023 <modouaichadiop@gmail.com>
2024-06-16 16:49:40 +0000

    chore: Fix text-transform value in globals.css and add hover animation to ellipsis button in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../public/images/Logo.png";
 import {
   MoreHorizontal,
@@ -20,8 +20,14 @@ import LangSwitcher from "./LangSwitcher";
 //   ssr: false,
 // });
 export default function Header() {
+  // const [isMenuOpen, SetIsMenuOpen] = useState(false);
+
+  // const handleOpenMenu = () => {
+  //   SetIsMenuOpen(!isMenuOpen);
+  //   console.log("it's work");
+  // };
   return (
-    <header className="flex w-full  max-w-7xl items-center justify-between p-8 fixed top-0 z-10 backdrop-blur-sm">
+    <header className="flex w-full max-w-7xl items-center justify-between p-8 fixed top-0 z-10 backdrop-blur-sm">
       <Link href="/">
         <Image src={logo} alt="avatar" height={40} />
       </Link>
@@ -37,25 +43,42 @@ export default function Header() {
               alt="Ball"
             />
           </div>
-          <span>23</span>
+          <span className="hover:scale-125 transition duration-200 cursor-pointer">
+            23
+          </span>
         </div>
         <div className="flex gap-1">
-          <Link href="https://github.com/modouaicha023">
+          <Link
+            href="https://github.com/modouaicha023"
+            className="hover:scale-125 transition duration-200 cursor-pointer"
+          >
             <Github
               size={16}
               className="hover:scale-125 transition duration-200 cursor-pointer"
             />
           </Link>
-          <Link href="https://www.linkedin.com/in/modouaicha023">
+          <Link
+            href="https://www.linkedin.com/in/modouaicha023"
+            className="hover:scale-125 transition duration-200 cursor-pointer"
+          >
             <Linkedin size={16} />
           </Link>
-          <Link href="https://www.youtube.com/@modouaicha023">
+          <Link
+            href="https://www.youtube.com/@modouaicha023"
+            className="hover:scale-125 transition duration-200 cursor-pointer"
+          >
             <Youtube size={16} />
           </Link>
-          <Link href="https://twitter.com/modouaicha023">
+          <Link
+            href="https://twitter.com/modouaicha023"
+            className="hover:scale-125 transition duration-200 cursor-pointer"
+          >
             <Twitter size={16} />
           </Link>
-          <Link href="https://modouaicha023-cv.vercel.app/">
+          <Link
+            href="https://modouaicha023-cv.vercel.app/"
+            className="hover:scale-125 transition duration-200 cursor-pointer"
+          >
             <FileText size={16} />
           </Link>
         </div>
@@ -63,8 +86,30 @@ export default function Header() {
       <div className="flex items-center justify-center">
         <LangSwitcher />
         <ThemeToggle />
-        <MoreHorizontal size={48} className="cursor-not-allowed" />
+        <button>
+          
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="40"
+            height="40"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide hover:cursor-pointer lucide-ellipsis hover:animate-bounce-1 hover:animate-bounce-2 hover:animate-bounce-3"
+          >
+            <circle cx="12" cy="12" r="1" />
+            <circle cx="19" cy="12" r="1" />
+            <circle cx="5" cy="12" r="1" />
+          </svg>
+          
+        </button>
       </div>
+      {/* {isMenuOpen && (
+        <div className="fixed top-20 right-0 w-full h-24 bg-slate-300 transition duration-75 delay-75 "></div>
+      )} */}
     </header>
   );
 }
