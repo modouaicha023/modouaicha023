84447d882c7924d346dd31267a01acf2c2b03ea4
modouaicha023 <modouaichadiop@gmail.com>
2024-12-27 21:55:10 +0000

    update project list

--- src/app/api/data.jsx
+++ src/app/api/data.jsx
@@ -13,15 +13,6 @@ import galsendigitalstudio from "/public/images/galsen-digital-studio.png";
 
 export const projects = [
   {
-    name: "Galsen Digital Studio ",
-    image: galsendigitalstudio,
-    stack: ["Next.JS", "TypeScript", "Tailwind CSS"],
-    description:
-      "Galsen Digital Studio is a Next.js landing page styled with Tailwind CSS. It showcases the agency's expertise in development, marketing, and strategy, engaging potential clients with a visually appealing presentation of its services.",
-    url: "https://galsen-digital-studio.netlify.app/",
-    githubUrl: "https://github.com/modouaicha023/galsen-digital",
-  },
-  {
     name: "Galsen Coding Challenge",
     image: galsenCodingChallengeImage,
     stack: ["Next.JS", "TypeScript", "MongoDB"],
@@ -29,15 +20,6 @@ export const projects = [
       "This project aims to create a platform for coding challenges and collaboration within the Senegalese coding community. Users can participate in challenges, create their own, and engage in collaborative coding efforts.",
     url: "https://galsen-coding-challenge.vercel.app",
     githubUrl: "https://github.com/modouaicha023/GalsenCodingChallenge",
-  },
-  {
-    name: "Sama Keur",
-    image: samaKeurImage,
-    stack: ["React", "JavaScript", "MongoDB", "Express"],
-    description:
-      "SamaKeur is a user-friendly real estate platform where users can register using their email address or Google account. They have the ability to add, delete, and modify real estate rental or sale listings with ease. Find your dream home or a reliable tenant in the blink of an eye thanks to SamaKeur.",
-    url: "https://sama-keur-sn.onrender.com",
-    githubUrl: "https://github.com/modouaicha023/sama-keur-sn",
   },
   {
     name: "Game Of Life",
@@ -48,24 +30,6 @@ export const projects = [
     url: "https://modouaicha023.github.io/GameOfLife",
     githubUrl: "https://github.com/modouaicha023/GameOfLife",
   },
-  // {
-  //   name: "Tech Survey",
-  //   image: techSurveyImage,
-  //   stack: ["Next.JS", "JavaScript", "Firebase"],
-  //   description:
-  //     "The goal of this project is to establish a platform dedicated to conducting salary surveys for developers in Senegal. Users have the ability to participate in surveys, create their own surveys, and access all survey data within the platform.",
-  //   url: "https://tech-survey.vercel.app",
-  //   githubUrl: "https://github.com/modouaicha023/tech-survey",
-  // },
-  // {
-  //   name: "Blog App",
-  //   image: blogAppImage,
-  //   stack: ["Java", "SQLite"],
-  //   description:
-  //     "A very simple mobile application that allows users to add blogs, view blog details, developed with Java and SQLite for the database.",
-  //   url: "https://www.youtube.com/watch?v=cIkB6uH3T_4",
-  //   githubUrl: "https://github.com/modouaicha023/SimpleBlogApp",
-  // },
   {
     name: "Jayy Ma Baguette",
     image: jayyMaBaguetteImage,
@@ -74,42 +38,6 @@ export const projects = [
       "JayyMaBaguette is a landing page for a baker. It has 7 sections: the header, the category section, the about us section, the menu section, the product presentation section, the contact form, and the footer.",
     url: "https://modouaicha023.github.io/jaaymabaguette",
     githubUrl: "https://github.com/modouaicha023/jaaymabaguette",
-  },
-  {
-    name: "Sama Car",
-    image: samaCar,
-    stack: ["React", "Tailwind CSS"],
-    description:
-      "Sama Car is a landing page for a car business. It is a React application, styled using Tailwind CSS. The landing page aims to showcase the car business and its offerings to potential customers in an engaging and visually appealing manner.",
-    url: "https://sama-car.vercel.app",
-    githubUrl: "https://github.com/modouaicha023/sama-car",
-  },
-  {
-    name: "Sen Coffee",
-    image: senCoffee,
-    stack: ["React", "Tailwind CSS"],
-    description:
-      "Sen Coffee is a homepage for a coffee shop. It is a React application styled with Tailwind CSS. The purpose of this homepage is to present the coffee business and its products in an attractive and visually appealing way to potential customers.",
-    url: "https://sen-coffee.netlify.app/",
-    githubUrl: "https://github.com/modouaicha023/sen-coffee",
-  },
-  {
-    name: "Sama Contacts",
-    image: samaContactsImage,
-    stack: ["PHP", "JavaScript", "MySQL", "ajax"],
-    description:
-      "The Contacts Management System is a web application that allows users to manage their contacts. It provides functionalities such as adding new contacts, editing existing ones, and categorizing contacts into different categories.",
-    url: "https://github.com/modouaicha023/sama-contacts",
-    githubUrl: "https://github.com/modouaicha023/sama-contacts",
-  },
-  {
-    name: "Tic-Tac-Toe Game",
-    image: ticTacToeGame,
-    stack: ["HTML", "CSS", "JavaScript"],
-    description:
-      "Implemented a classic Tic Tac Toe game featuring a two-player mode, local storage for game persistence, and a clean user interface.",
-    url: "https://modouaicha023.github.io/Tic-Tac-Toe-Game",
-    githubUrl: "https://github.com/modouaicha023/Tic-Tac-Toe-Game",
   },
   {
     name: "Galsen Mangas",
@@ -122,77 +50,4 @@ export const projects = [
   },
 ];
 
-export const blogs = [
-  // {
-  //   name: "Galsen Coding Challenge",
-  //   image: galsenCodingChallengeImage,
-  //   stack: ["Next.JS", "TypeScript", "MongoDB"],
-  //   description:
-  //     "This project aims to create a platform for coding challenges and collaboration within the Senegalese coding community. Users can participate in challenges, create their own, and engage in collaborative coding efforts.",
-  //   url: "https://galsen-coding-challenge.vercel.app",
-  //   githubUrl: "https://github.com/modouaicha023/GalsenCodingChallenge",
-  // },
-  {
-    name: "Sama Keur",
-    image: samaKeurImage,
-    stack: ["React", "JavaScript", "MongoDB", "Express"],
-    description:
-      "SamaKeur is a user-friendly real estate platform where users can register using their email address or Google account. They have the ability to add, delete, and modify real estate rental or sale listings with ease. Find your dream home or a reliable tenant in the blink of an eye thanks to SamaKeur.",
-    url: "https://sama-keur-sn.onrender.com",
-    githubUrl: "https://github.com/modouaicha023/sama-keur-sn",
-  },
-  {
-    name: "Game Of Life",
-    image: gameOfLifeImage,
-    stack: ["JavaScript", "HTML", "CSS"],
-    description:
-      "This is a game called Game of life challenge. The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970.",
-    url: "https://modouaicha023.github.io/GameOfLife",
-    githubUrl: "https://github.com/modouaicha023/GameOfLife",
-  },
-  // {
-  //   name: "Tech Survey",
-  //   image: techSurveyImage,
-  //   stack: ["Next.JS", "JavaScript", "Firebase"],
-  //   description:
-  //     "The goal of this project is to establish a platform dedicated to conducting salary surveys for developers in Senegal. Users have the ability to participate in surveys, create their own surveys, and access all survey data within the platform.",
-  //   url: "https://tech-survey.vercel.app",
-  //   githubUrl: "https://github.com/modouaicha023/tech-survey",
-  // },
-  // {
-  //   name: "Blog App",
-  //   image: blogAppImage,
-  //   stack: ["Java", "SQLite"],
-  //   description:
-  //     "A very simple mobile application that allows users to add blogs, view blog details, developed with Java and SQLite for the database.",
-  //   url: "https://www.youtube.com/watch?v=cIkB6uH3T_4",
-  //   githubUrl: "https://github.com/modouaicha023/SimpleBlogApp",
-  // },
-  {
-    name: "Jayy Ma Baguette",
-    image: jayyMaBaguetteImage,
-    stack: ["HTML", "CSS"],
-    description:
-      "JayyMaBaguette is a landing page for a baker. It has 7 sections: the header, the category section, the about us section, the menu section, the product presentation section, the contact form, and the footer.",
-    url: "https://modouaicha023.github.io/jaaymabaguette",
-    githubUrl: "https://github.com/modouaicha023/jaaymabaguette",
-  },
-  {
-    name: "Sama Contacts",
-    image: samaContactsImage,
-    stack: ["PHP", "JavaScript", "MySQL", "ajax"],
-    description:
-      "The Contacts Management System is a web application that allows users to manage their contacts. It provides functionalities such as adding new contacts, editing existing ones, and categorizing contacts into different categories.",
-    url: "https://sama-contacts.000webhostapp.com",
-    githubUrl: "https://github.com/modouaicha023/sama-contacts",
-  },
-  {
-    name: "Tic-Tac-Toe Game",
-    image: ticTacToeGame,
-    stack: ["HTML", "CSS", "JavaScript"],
-    description:
-      "Implemented a classic Tic Tac Toe game featuring a two-player mode, local storage for game persistence, and a clean user interface.",
-    url: "https://modouaicha023.github.io/Tic-Tac-Toe-Game",
-    githubUrl: "https://github.com/modouaicha023/Tic-Tac-Toe-Game",
-  },
-];
+export const blogs = [];
