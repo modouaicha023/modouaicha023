58f7fe67d223fb9db1637ee97a49bea0e0dd7aa7
modouaicha023 <modouaichadiop@gmail.com>
2025-01-26 18:04:21 +0000

    init

--- src/components/ui/Container.jsx
+++
@@ -1,7 +0,0 @@
-export default function Container({ children }) {
-  return (
-    <main className="flex min-h-fit w-screen  items-center justify-center p-4 bkpgrid:overflow-hidden">
-      {children}
-    </main>
-  );
-}
--- src/components/ui/Section.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Section({ children }) {
-  return (
-    <section className=" min-h-fit w-full max-w-7xl sm:min-w-[300px] mx-auto p-2 rounded-lg bkpgrid:overflow-hidden">
-      {children}
-    </section>
-  );
-}
